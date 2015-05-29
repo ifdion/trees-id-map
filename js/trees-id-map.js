d9cf39d4c7b946c2528a4a841b9b74ff069ca93c
Dion Priyandiono <ifdion@gmail.com>
2015-05-29 10:01:51 +0700

    enable get data from data-tree_offset attribute

--- js/trees-id-map.js
+++ js/trees-id-map.js
@@ -472,6 +472,9 @@ function treeMap(elementID){
 	} else {
 		var lotID = mapObject.getAttribute('data-lot_id');
 		var offset = mapObject.getAttribute('data-offset');
+		if (!offset) {
+			var offset = mapObject.getAttribute('data-tree_offset');
+		}
 		var arrayLength = offset.split(',').length;
 		var APIurl = 'http://api.trees.id/?object=tree&callback=callback&lot_id=' + lotID +'&tree_offset='+offset +'&per_page='+arrayLength;
 	}
